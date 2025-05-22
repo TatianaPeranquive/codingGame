078d27f981346649efe9489019a475fd98b66737
TatianaPeranquive <tatianaperanquive@gmail.com>
2025-05-21 20:50:51 -0500

    geometric mean of two numbers

---
+++ Thor_game/index7.php
@@ -0,0 +1,11 @@
+<?php
+/* Challenge: 
+You  must to find the geometric mean of two numbers. 
+It is a square root of multiplying both numbers
+*/
+$a = intval(fgets(STDIN));
+$b = intval(fgets(STDIN));
+
+echo sqrt($a*$b);
+
+?>
